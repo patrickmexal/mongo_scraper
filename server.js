defd2a6e5e9f33aa4359c047b7c004b7231a252b
Patrick Mexal <patrickmexal@gmail.com>
2018-03-13 10:29:01 -0700

    still grinding on scraper mechanics

--- server.js
+++ server.js
@@ -1,5 +1,14 @@
+// Dependencies
+var express = require("express");
+var mongojs = require("mongojs");
+// Scraping Dependencies
 var cheerio = require("cheerio");
 var request = require("request");
+// Initialize Express
+var app = express();
+// Database configuration
+var databaseUrl = "arizonaSports";
+var collections = ["sportsNews"];
 
 // First, tell the console what server.js is doing
 console.log("\n******************************************\n" +
@@ -8,12 +17,12 @@ console.log("\n******************************************\n" +
             "\n******************************************\n");
 
 // Making a request for arizonasports.com homepage
-request("https://www.arizonasports.com/", function(error, response, html) {
+  request("https://www.arizonasports.com/", function(error, response, html) {
 
   // Load the body of the HTML into cheerio
   var $ = cheerio.load(html);
 
-  // Empty array to save our scraped data
+  // Blank array to save our scraped data
   var results = [];
 
   // With cheerio, find each list item that is a link.
@@ -22,16 +31,16 @@ request("https://www.arizonasports.com/", function(error, response, html) {
     // Save the text of the h4-tag as "title"
     var title = $(element).text();
 
-    // Find the h4 tag's parent a-tag, and save it's href value as "link"
+    // Find the link tag's parent a-tag, and save it's href value as "link"
     var link = $(element).parent().attr("href");
 
-    // Make an object with data we scraped for this h4 and push it to the results array
+    // Make an object with data we scraped push to results array
     results.push({
       title: title,
       link: link
     });
   });
 
-  // After looping through each h4.headline-link, log the results
+  // After looping through each li log the results
   console.log(results);
 });
