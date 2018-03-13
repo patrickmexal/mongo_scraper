ccb9499151347f313b75d469660114715773b5d2
Patrick Mexal <patrickmexal@gmail.com>
2018-03-13 10:01:54 -0700

    messing with the scrape

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 var cheerio = require("cheerio");
 var request = require("request");
 
-// First, tell the console what server2.js is doing
+// First, tell the console what server.js is doing
 console.log("\n******************************************\n" +
             "Grabbing every article headline and link\n" +
-            "from the NHL website:" +
+            "from Arizona Sports" +
             "\n******************************************\n");
 
-// Making a request for nhl.com's homepage
+// Making a request for arizonasports.com homepage
 request("https://www.arizonasports.com/", function(error, response, html) {
 
   // Load the body of the HTML into cheerio
@@ -16,8 +16,8 @@ request("https://www.arizonasports.com/", function(error, response, html) {
   // Empty array to save our scraped data
   var results = [];
 
-  // With cheerio, find each h4-tag with the class "headline-link" and loop through the results
-  $("li").each(function(i, element) {
+  // With cheerio, find each list item that is a link.
+  $("li a").each(function(i, element) {
 
     // Save the text of the h4-tag as "title"
     var title = $(element).text();
